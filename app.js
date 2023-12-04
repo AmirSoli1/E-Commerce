65bf9eaa4889287d66d3e2b28128e93d24991808
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-12-04 20:05:04 +0200

    add starter functions and connect to db

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const connect = require('./db/connect');
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connect(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
