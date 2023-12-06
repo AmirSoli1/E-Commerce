cf1bcdd11cb82067f209a51f2e2fdde78be61404
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-12-06 18:15:49 +0200

    add register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,17 @@
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const User = require('..//Models/User');
+
 const register = async (req, res) => {
-  res.send('register');
+  const { name, email, password } = req.body;
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    throw new BadRequestError('Email already exists');
+  }
+  const firstUser = (await User.countDocuments({})) === 0;
+  const role = firstUser ? 'admin' : 'user';
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
