8cac65f41720ddbb43714a59c929e514697a6df7
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-12-13 19:00:07 +0200

    add review model

---
+++ Models/Review.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide review title'],
+    },
+    comment: {
+      type: String,
+      required: [true, 'Please provide review text'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.Model('Review', reviewSchema);
