c093084442294dc34ef4e9e3a50add60cb6ba30a
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-12-14 14:08:52 +0200

    add average rating calculator

--- Models/Product.js
+++ Models/Product.js
@@ -56,6 +56,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: 'User',
@@ -71,5 +75,13 @@ productSchema.virtual('reviews', {
   foreignField: 'product',
   justOne: false,
 });
+
+productSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function () {
+    await this.model('Review').deleteMany({ product: this._id });
+  }
+);
 
 module.exports = mongoose.model('Product', productSchema);
